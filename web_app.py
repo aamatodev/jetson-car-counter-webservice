231d97762114e7a990206daea421f7f21d1eee45
Alessandro Amato <aa357@students.uwf.edu>
2024-06-28 20:09:31 -0500

    feat: basic webapp

---
+++ web_app.py
@@ -0,0 +1,25 @@
+import json
+
+import requests
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/data', methods=['POST'])
+def data():
+    url = 'http://10.100.4.58:5000/get-current-car-counter'
+
+    x = requests.get(url).json()
+
+    return jsonify(x)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
